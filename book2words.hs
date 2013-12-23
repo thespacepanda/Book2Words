1085cad7140a3d41674bda381765a6458df1bf19
Britt Mathis <britt.mathis@gmail.com>
2013-12-22 23:39:37 -0500

    Enacted changes suggested by 5outh on /r/haskell; looking into Text instead of String as suggested by tWoolie

--- book2words.hs
+++ book2words.hs
@@ -1,11 +1,11 @@
 import System.Environment (getArgs)
 import System.FilePath (takeBaseName)
 import Data.Char (toLower)
-import Data.List (nub, sortBy, intersperse)
+import Data.List (nub, sortBy, intersperse, group, sort)
 import Data.Function (on)
 
 main :: IO ()
-main = getArgs >>= mapM_ (\arg -> makeWordFile arg)
+main = getArgs >>= mapM_ makeWordFile
 
 makeWordFile :: FilePath -> IO ()
 makeWordFile book = do
@@ -13,26 +13,20 @@ makeWordFile book = do
   writeFile (takeBaseName book ++ ".words") (parseBook content)
 
 parseBook :: String -> String
-parseBook = prettyPrint . orderWords . getWords
+parseBook = unlines . orderWords . getWords
 
 getWords :: String -> [String]
-getWords book = filter notPunctuated $ words $ map toLower book
+getWords = filter notPunctuated . words . map toLower
 
 notPunctuated :: String -> Bool
 notPunctuated = all (`notElem` "¿¡—«»[]{}*.?!,(:-;’\'\"\")\"\"'")
 
 orderWords :: [String] -> [String]
-orderWords = discardCount . sortWords . getFrequency
+orderWords = map fst . sortWords . getFrequency
 
 getFrequency :: [String] -> [(String, Int)]
-getFrequency wordList = [ (word, length $ filter (== word) wordList)
-                     | word <- nub wordList ]
+getFrequency list = map (\x -> (head x, length x) ) grouped
+  where grouped = group . sort $ list
 
 sortWords :: [(String, Int)] -> [(String, Int)]
-sortWords = reverse . sortBy (compare `on` snd)
-
-discardCount :: [(String, Int)] -> [String]
-discardCount = map fst
-
-prettyPrint :: [String] -> String
-prettyPrint = concat . intersperse "\n"
+sortWords = sortBy (flip compare `on` snd)
